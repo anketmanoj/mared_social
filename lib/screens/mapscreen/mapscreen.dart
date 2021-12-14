a60c145352d6b13dc3fe49cd8413c7c079113221
anketmanoj <anket.m@live.com>
2021-12-14 15:00:02 +0400

    backup running

--- lib/screens/mapscreen/mapscreen.dart
+++ lib/screens/mapscreen/mapscreen.dart
@@ -1,106 +1,69 @@
-import 'dart:typed_data';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_place/google_place.dart';
+import 'package:mared_social/screens/mapscreen/mapscreenhelper.dart';
+import 'package:provider/provider.dart';
 
 class MapScreen extends StatefulWidget {
   @override
-  State<MapScreen> createState() => _MapScreenState();
+  _MapScreenState createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
-  GooglePlace googlePlace =
-      GooglePlace("AIzaSyCMYWYXY7CM6l3axYkOjHIqtuSUsTKbGAs");
-  List<AutocompletePrediction> predictions = [];
-  late GoogleMapController mapController;
-  late DetailsResult detailsResult;
+  late GoogleMapController googleMapController;
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
 
-  void autoCompleteSearch(String value) async {
-    var result = await googlePlace.queryAutocomplete.get(value);
-    if (result != null && result.predictions != null && mounted) {
-      setState(() {
-        predictions = result.predictions!;
-      });
-    }
+  void initMarker({required dynamic specify, required String specifyId}) async {
+    var markerIdVal = specifyId;
+    final MarkerId markerId = MarkerId(markerIdVal);
+
+    final Marker marker = Marker(
+      markerId: markerId,
+      position:
+          LatLng(double.parse(specify['lat']), double.parse(specify['lng'])),
+      infoWindow: InfoWindow(
+        title: specify['caption'],
+        onTap: () {
+          Provider.of<MapScreenHelper>(context, listen: false)
+              .showDetails(context: context, documentSnapshot: specify);
+        },
+        snippet: specify['description'],
+      ),
+    );
+    setState(() {
+      markers[markerId] = marker;
+    });
+  }
+
+  getMarkerData() async {
+    FirebaseFirestore.instance.collection("posts").get().then((posts) {
+      for (int i = 0; i < posts.docs.length; i++) {
+        initMarker(specify: posts.docs[i], specifyId: posts.docs[i].id);
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    getMarkerData();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Search",
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blue,
-                    width: 2.0,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.black54,
-                    width: 2.0,
-                  ),
-                ),
-              ),
-              onChanged: (value) {
-                if (value.isNotEmpty) {
-                  autoCompleteSearch(value);
-                } else {
-                  if (predictions.length > 0 && mounted) {
-                    setState(() {
-                      predictions = [];
-                    });
-                  }
-                }
-              },
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: predictions.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: CircleAvatar(
-                      child: Icon(
-                        Icons.pin_drop,
-                        color: Colors.white,
-                      ),
-                    ),
-                    title: Text(predictions[index].description!),
-                    onTap: () {
-                      getDetils(
-                        googlePlace: googlePlace,
-                        placeId: predictions[index].placeId!,
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
-          ],
+      body: GoogleMap(
+        mapType: MapType.satellite,
+        markers: Set<Marker>.of(markers.values),
+        myLocationEnabled: true,
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(25.203809493644336, 55.27576934136506),
+          zoom: 10,
         ),
+        onMapCreated: (controller) {
+          googleMapController = controller;
+        },
       ),
     );
-  }
-
-  void getDetils(
-      {required String placeId, required GooglePlace googlePlace}) async {
-    var result = await googlePlace.details.get(placeId);
-    if (result != null && result.result != null && mounted) {
-      setState(() {
-        detailsResult = result.result!;
-      });
-
-      print(detailsResult.geometry!.location!.lat);
-      print(detailsResult.geometry!.location!.lng);
-    }
   }
 }
