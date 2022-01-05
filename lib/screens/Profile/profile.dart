dcf3aae3356b6a56c31b2035c70a531c95770acb
anketmanoj <anket.m@live.com>
2022-01-05 15:33:02 +0400

    profile scroll fixed. now to fix altprofile

--- lib/screens/Profile/profile.dart
+++ lib/screens/Profile/profile.dart
@@ -1,3 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:card_swiper/card_swiper.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
@@ -13,7 +15,9 @@ class Profile extends StatelessWidget {
   Profile({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: constantColors.blueGreyColor,
       appBar: AppBar(
         centerTitle: true,
         actions: [
@@ -68,45 +72,113 @@ class Profile extends StatelessWidget {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: constantColors.blueGreyColor.withOpacity(0.6),
-            ),
-            child: StreamBuilder<DocumentSnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection("users")
-                  .doc(Provider.of<Authentication>(context, listen: false)
-                      .getUserId)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else {
-                  return Column(
-                    children: [
-                      Provider.of<ProfileHelpers>(context, listen: false)
-                          .headerProfile(context, snapshot),
-                      Provider.of<ProfileHelpers>(context, listen: false)
-                          .divider(),
-                      Provider.of<ProfileHelpers>(context, listen: false)
-                          .middleProfile(context, snapshot),
-                      Provider.of<ProfileHelpers>(context, listen: false)
-                          .footerProfile(context, snapshot),
-                    ],
-                  );
-                }
-              },
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: size.height * 0.43,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                color: constantColors.blueGreyColor,
+                child: StreamBuilder<DocumentSnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection("users")
+                      .doc(Provider.of<Authentication>(context, listen: false)
+                          .getUserId)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else {
+                      return Column(
+                        children: [
+                          Provider.of<ProfileHelpers>(context, listen: false)
+                              .headerProfile(context, snapshot),
+                          Provider.of<ProfileHelpers>(context, listen: false)
+                              .divider(),
+                          Provider.of<ProfileHelpers>(context, listen: false)
+                              .middleProfile(context, snapshot),
+                        ],
+                      );
+                    }
+                  },
+                ),
+              ),
             ),
           ),
-        ),
+          StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection("users")
+                .doc(Provider.of<Authentication>(context, listen: false)
+                    .getUserId)
+                .collection("posts")
+                .orderBy("time", descending: true)
+                .snapshots(),
+            builder: (context, userPostSnap) {
+              return SliverPadding(
+                padding: const EdgeInsets.all(4),
+                sliver: SliverGrid(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 5,
+                    mainAxisSpacing: 5,
+                  ),
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      if (index.toInt() < userPostSnap.data!.docs.length) {
+                        var userPostDocSnap = userPostSnap.data!.docs[index];
+                        return InkWell(
+                          onTap: () {
+                            Provider.of<ProfileHelpers>(context, listen: false)
+                                .showPostDetail(
+                                    context: context,
+                                    documentSnapshot: userPostDocSnap);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: SizedBox(
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(5),
+                                child: Swiper(
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    return CachedNetworkImage(
+                                      fit: BoxFit.cover,
+                                      imageUrl: userPostDocSnap['imageslist']
+                                          [index],
+                                      progressIndicatorBuilder:
+                                          (context, url, downloadProgress) =>
+                                              SizedBox(
+                                        height: 50,
+                                        width: 50,
+                                        child: LoadingWidget(
+                                            constantColors: constantColors),
+                                      ),
+                                      errorWidget: (context, url, error) =>
+                                          const Icon(Icons.error),
+                                    );
+                                  },
+                                  itemCount:
+                                      (userPostDocSnap['imageslist'] as List)
+                                          .length,
+                                  itemHeight:
+                                      MediaQuery.of(context).size.height * 0.3,
+                                  itemWidth: MediaQuery.of(context).size.width,
+                                  layout: SwiperLayout.DEFAULT,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
